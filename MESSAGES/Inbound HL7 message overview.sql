ee3032bcb63825e54cdc8f88e6c164e225063ebb
neoversionsix <you@example.com>
2026-05-06 14:21:21 +1000

    feat: add SQL query for overview of recent inbound HL7 messages from ESI_LOG

---
+++ MESSAGES/Inbound HL7 message overview.sql
@@ -0,0 +1,17 @@
+SELECT
+/*This query is for a rough overview of incoming messages into Cerner EMR*/
+	E.MSH_SENDING_APP
+	, TO_CHAR(
+        E.CREATE_DT_TM AT TIME ZONE 'Australia/Melbourne', 
+        'YYYY-MM-DD HH24:MI:SS'
+    ) AS CREATED
+	, E.ERROR_STAT -- Did it load?
+	, E.ERROR_TEXT -- load errors eg no order id
+	, E.MSH_MSG_TYPE AS MESSAGE_TYPE
+
+FROM
+	WESTERNHEALTH_P2031.ESI_LOG E
+
+WHERE CAST(E.CREATE_DT_TM AT TIME ZONE 'Australia/Melbourne' AS TIMESTAMP) 
+      > CAST(SYSTIMESTAMP AT TIME ZONE 'Australia/Melbourne' AS TIMESTAMP) - INTERVAL '3' DAY
+      AND ROWNUM <= 1000
